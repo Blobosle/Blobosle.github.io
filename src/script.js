b042573f866d332e3285e6825111d7dc191239a1
Benjamin Lobos <blobosle@purdue.edu>
2025-04-07 22:41:33 -0400

    Fleshed out animations

---
+++ src/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("mousemove", function(event) {
+    const aboutSection = document.querySelector(".about");
+
+    const offsetX = (event.clientX / window.innerWidth - 0.5) * 15;
+    const offsetY = (event.clientY / window.innerHeight - 0.5) * 15;
+
+    aboutSection.style.backgroundPosition =
+        `calc(50% + ${offsetX}px) calc(50% + ${offsetY}px)`;
+});
+
